cac38c59965a77d520a323897f6913598e29edb4
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2022-01-14 11:08:19 -0700

    1/14/22 SpaceShooter Production start

---
+++ Assets/SpaceShooter/Scripts/SpacePlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacePlayer : MonoBehaviour
+{
+    [Range(0, 100)] [Tooltip("Speed of the player ship")] public float speed = 40;
+
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
