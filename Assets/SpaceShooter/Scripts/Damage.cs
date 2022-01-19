30a8ea062e5f8f3fd9f2441e76ad7b93ddcad625
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2022-01-19 11:04:49 -0700

    1/19/22 Explosion collision stuff

---
+++ Assets/SpaceShooter/Scripts/Damage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField] float damage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out Health health))
+        {
+            health.Damage(damage);
+        }
+        if (gameObject.TryGetComponent(out health) && other.gameObject.TryGetComponent(out Damage otherDamage))
+        {
+            health.Damage(otherDamage.damage);
+        }
+    }
+}
